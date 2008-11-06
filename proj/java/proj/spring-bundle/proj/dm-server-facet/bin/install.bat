68538c28bf92351a178725579b62d2d148d5fba0
n0rad <n0rad@16364050-63f9-42cc-b797-5db5df9659b9>
2008-11-06 16:41:57 +0000

    - adding dm-server-facet project (not used in current version) - updating eclipse conf
    git-svn-id: http://repos.awired.net/svn/parent/trunk@50 16364050-63f9-42cc-b797-5db5df9659b9

---
+++ proj/java/proj/spring-bundle/proj/dm-server-facet/bin/install.bat
@@ -0,0 +1,2 @@
+call mvn install:install-file -Dfile=../src/dm-server-facet.jar -DgroupId=net.awired.parent.java.spring-bundle -DartifactId=dm-server-facet -Dpackaging=jar -Dversion=1.0 -DgeneratePom=true -DcreateChecksum=true
+pause
